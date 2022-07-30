27d2c525d70b9842f2904b780372702df30f5350
albertstevensen <albertst2403@gmail.com>
2022-07-30 18:36:22 +0700

    Create Test Suites Task

---
+++ Test Suites/Automated Testing Samsung Shop.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Automated Testing Samsung Shop</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6d73517e-2ba3-4956-8957-3da0bfb0fb92</testSuiteGuid>
+   <testCaseLink>
+      <guid>a9976b4f-76db-40e5-b860-0b3c0e013569</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Samsung_Shop/Buy_Samsung</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
